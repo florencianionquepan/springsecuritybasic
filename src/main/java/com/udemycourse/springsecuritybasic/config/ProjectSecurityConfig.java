11ec42b75e0cf01171808a025debd37adf0c2567
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-27 15:44:15 -0300

    Update ProjectSecurityConfig.java

--- src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,35 +1,31 @@
 package com.udemycourse.springsecuritybasic.config;
 
-import org.hibernate.boot.model.relational.Database;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll())
+        http.csrf((csrf) -> csrf.disable())
+            .authorizeHttpRequests((requests)->requests
+                        .requestMatchers("/myAccount/**","/myBalance/**","/myLoans/**","/myCards/**").authenticated()
+                        .requestMatchers("/notices","/contact","/register").permitAll())
         .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
-    @Bean
+/*    @Bean
     public UserDetailsService userDetailsService(DataSource datasource){
         return new JdbcUserDetailsManager(datasource);
-    }
+    }*/
 
     @Bean
     //por ahora las voy a almacenar en tezto plano
