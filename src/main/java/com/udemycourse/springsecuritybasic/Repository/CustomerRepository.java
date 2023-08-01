1437d336231e4d85d97601a10e4570ae4540ed5a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-01 18:15:11 -0300

    Update CustomerRepository.java

--- src/main/java/com/udemycourse/springsecuritybasic/Repository/CustomerRepository.java
+++ src/main/java/com/udemycourse/springsecuritybasic/Repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.udemycourse.springsecuritybasic.Repository;
 
 import com.udemycourse.springsecuritybasic.model.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer,Long> {
 
     List<Customer> findByEmail(String email);
