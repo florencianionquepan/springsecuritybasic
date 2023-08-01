e442f4c5f141fa0fa1b6002ff23d8d2f580cba06
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-01 18:15:22 -0300

    Create LoanRepository.java

---
+++ src/main/java/com/udemycourse/springsecuritybasic/Repository/LoanRepository.java
@@ -0,0 +1,12 @@
+package com.udemycourse.springsecuritybasic.Repository;
+
+import com.udemycourse.springsecuritybasic.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends CrudRepository<Loans,Long> {
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+}
