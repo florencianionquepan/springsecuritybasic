b0f5045ee9d28ac7aa651255929366a5d7164919
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-01 18:29:51 -0300

    agrego endpoint /user para auth

--- src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -16,7 +16,7 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
             .authorizeHttpRequests((requests)->requests
-                        .requestMatchers("/myAccount/**","/myBalance/**","/myLoans/**","/myCards/**").authenticated()
+                        .requestMatchers("/myAccount/**","/myBalance/**","/myLoans/**","/myCards/**","/user").authenticated()
                         .requestMatchers("/notices","/contact","/register").permitAll())
         .formLogin(Customizer.withDefaults())
         .httpBasic(Customizer.withDefaults());
