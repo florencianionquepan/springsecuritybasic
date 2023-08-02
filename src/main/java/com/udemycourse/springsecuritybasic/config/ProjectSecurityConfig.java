31c9ec9487a7f515cbaa32530917cf77daf69688
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-02 11:59:53 -0300

    se agrega configuración de cors

--- src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.udemycourse.springsecuritybasic.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,13 +9,30 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable())
+        http.cors((cors)->cors.configurationSource(new CorsConfigurationSource() {
+            //anonymous inner class
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config=new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+        .csrf((csrf) -> csrf.disable())
             .authorizeHttpRequests((requests)->requests
                         .requestMatchers("/myAccount/**","/myBalance/**","/myLoans/**","/myCards/**","/user").authenticated()
                         .requestMatchers("/notices","/contact","/register").permitAll())
