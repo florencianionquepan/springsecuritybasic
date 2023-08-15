0c4493715ab2c069006f98896c7bd01b6bac7ce0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-15 11:53:24 -0300

    agrega config. para trabajar con jwt

--- src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/udemycourse/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -5,24 +5,21 @@ import com.udemycourse.springsecuritybasic.filter.AuthoritiesLoggingAtFilter;
 import com.udemycourse.springsecuritybasic.filter.CsrfCookieFilter;
 import com.udemycourse.springsecuritybasic.filter.RequestValidationBeforeFilter;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.function.Supplier;
 
@@ -34,10 +31,8 @@ public class ProjectSecurityConfig {
         CsrfTokenRequestAttributeHandler requestHandler= new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName("_csrf");
 
-        http.securityContext(httpSecuritySecurityContextConfigurer -> {
-            httpSecuritySecurityContextConfigurer.requireExplicitSave(false);
-                }).sessionManagement(httpSecuritySessionManagementConfigurer -> {
-                    httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+        http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+                    httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
         .cors((cors)->cors.configurationSource(new CorsConfigurationSource() {
             //anonymous inner class
@@ -48,6 +43,8 @@ public class ProjectSecurityConfig {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        //QUE EL CLIENTE ACEPTE LO QUE LE VOY A MANDAR POR EL HEADER
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
